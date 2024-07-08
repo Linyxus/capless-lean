81f584d4ab40b5cd0cfbb60a327559ae063b2513
Yichen Xu <yichen.x@outlook.com>
2024-07-08 17:16:40 +0100

    Add index file

--- Capless.lean
+++ Capless.lean
@@ -1,3 +1,30 @@
--- This module serves as the root of the `CaplessLean` library.
--- Import modules here that should be built as part of the library.
 import «Capless».Basic
+
+-- Syntax
+import «Capless».CaptureSet
+import «Capless».Type
+import «Capless».Term
+import «Capless».Context
+
+-- Static semantics
+import «Capless».Subcapturing
+import «Capless».Subtyping
+import «Capless».Subtyping
+
+-- Operational semantics
+import «Capless».Store
+import «Capless».Reduction
+
+-- Renaming
+--- Term
+import «Capless».Renaming.Term.Subcapturing
+import «Capless».Renaming.Term.Subtyping
+import «Capless».Renaming.Term.Typing
+--- Type
+import «Capless».Renaming.Type.Subcapturing
+import «Capless».Renaming.Type.Subtyping
+import «Capless».Renaming.Type.Typing
+--- Capture
+import «Capless».Renaming.Capture.Subcapturing
+import «Capless».Renaming.Capture.Subtyping
+import «Capless».Renaming.Capture.Typing
