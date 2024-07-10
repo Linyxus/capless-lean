b1f004bdd054169861b05efff6d111a1a6ceda0f
Yichen Xu <yichen.x@outlook.com>
2024-07-10 17:03:50 +0200

    term subst: finish typing

--- Capless/Subst/Term/Typing.lean
+++ Capless/Subst/Term/Typing.lean
@@ -1,10 +1,122 @@
 import Capless.Typing
 import Capless.Subst.Basic
+import Capless.Subst.Term.Subtyping
+import Capless.Renaming.Term.Typing
 namespace Capless
 
 theorem Typed.subst
+  {Γ : Context n m k} {Δ : Context n' m k}
   (h : Typed Γ t E)
   (σ : VarSubst Γ f Δ) :
-  Typed Δ (t.rename f) (E.rename f) := sorry
+  Typed Δ (t.rename f) (E.rename f) := by
+  induction h generalizing n'
+  case var hb =>
+    simp [Term.rename, EType.rename]
+    apply σ.map
+    trivial
+  case pack ih =>
+    simp [Term.rename, EType.rename]
+    apply pack
+    have ih := ih σ
+    simp [EType.rename] at ih
+    rw [CType.copen_rename_comm] at ih
+    exact ih
+  case sub hs ih =>
+    apply sub
+    { apply ih; trivial }
+    { apply hs.subst; trivial }
+  case abs hc ih =>
+    simp [Term.rename, EType.rename, CType.rename, SType.rename]
+    apply abs
+    { apply ih
+      apply σ.ext }
+    { have hc1 := hc.rename (f := f)
+      simp [Term.rename] at hc1
+      exact hc1 }
+  case tabs hc ih =>
+    simp [Term.rename, EType.rename, CType.rename, SType.rename]
+    apply tabs
+    { apply ih
+      apply σ.text }
+    { have hc1 := hc.rename (f := f)
+      simp [Term.rename] at hc1
+      exact hc1 }
+  case cabs hc ih =>
+    simp [Term.rename, EType.rename, CType.rename, SType.rename]
+    apply cabs
+    { apply ih
+      apply σ.cext }
+    { have hc1 := hc.rename (f := f)
+      simp [Term.rename] at hc1
+      exact hc1 }
+  case app ih1 ih2 =>
+    simp [Term.rename]
+    rw [EType.rename_open]
+    apply app
+    { have ih1 := ih1 σ
+      simp [Term.rename, EType.rename, CType.rename, SType.rename] at ih1
+      exact ih1 }
+    { have ih2 := ih2 σ
+      simp [Term.rename, EType.rename, CType.rename, SType.rename] at ih2
+      exact ih2 }
+  case tapp ih =>
+    simp [Term.rename]
+    rw [EType.rename_topen]
+    apply tapp
+    have ih1 := ih σ
+    simp [Term.rename, EType.rename, CType.rename, SType.rename] at ih1
+    exact ih1
+  case capp ih =>
+    simp [Term.rename]
+    rw [EType.rename_copen]
+    apply capp
+    have ih1 := ih σ
+    simp [Term.rename, EType.rename, CType.rename, SType.rename] at ih1
+    exact ih1
+  case box ih =>
+    simp [Term.rename, EType.rename, CType.rename, SType.rename]
+    apply box
+    have ih1 := ih σ
+    simp [Term.rename, EType.rename, CType.rename, SType.rename] at ih1
+    exact ih1
+  case unbox ih =>
+    simp [Term.rename, EType.rename, CType.rename, SType.rename]
+    apply unbox
+    have ih1 := ih σ
+    simp [Term.rename, EType.rename, CType.rename, SType.rename] at ih1
+    exact ih1
+  case letin ih1 ih2 =>
+    simp [Term.rename]
+    apply letin
+    { have ih1 := ih1 σ
+      simp [EType.rename] at ih1
+      exact ih1 }
+    { have ih2 := ih2 (σ.ext _)
+      rw [<- EType.weaken_rename] at ih2
+      exact ih2 }
+  case letex ih1 ih2 =>
+    simp [Term.rename]
+    apply letex
+    { have ih1 := ih1 σ
+      simp [EType.rename] at ih1
+      exact ih1 }
+    { have ih2 := ih2 (σ.cext.ext _)
+      rw [<- EType.weaken_rename] at ih2
+      rw [EType.cweaken_rename_comm] at ih2
+      exact ih2 }
+  case bindt ih =>
+    simp [Term.rename]
+    apply bindt
+    have ih := ih σ.text
+    rw [EType.tweaken_rename] at ih
+    simp [TBinding.rename] at ih
+    exact ih
+  case bindc ih =>
+    simp [Term.rename]
+    apply bindc
+    have ih := ih σ.cext
+    rw [EType.cweaken_rename_comm] at ih
+    simp [CBinding.rename] at ih
+    exact ih
 
 end Capless
