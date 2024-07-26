b7936b4ddc177223f6c7a8ab8c1cb2e1c184aad6
Yichen Xu <yichen.x@outlook.com>
2024-07-26 17:14:25 +0100

    Some cleanups

--- Capless/Typing/Basic.lean
+++ Capless/Typing/Basic.lean
@@ -4,12 +4,6 @@ import Capless.Subcapturing.Basic
 import Capless.Subtyping.Basic
 namespace Capless
 
--- inductive EType.TryOpen : Fin n -> EType n m k -> CType n m k -> Prop where
--- | type :
---   TryOpen x (EType.type T) T
--- | exist :
---   TryOpen x (EType.exist T) (T.)
-
 theorem Typing.inv_subcapt'
   (he1 : t0 = Term.var x) (he2 : E0 = EType.type (CType.capt C S))
   (h : Typed Γ t0 E0) :
@@ -18,9 +12,6 @@ theorem Typing.inv_subcapt'
   case var =>
     cases he1; cases he2
     apply Subcapt.refl
-  -- case exists_elim =>
-  --   cases he1; cases he2
-  --   apply Subcapt.refl
   case sub hsub ih =>
     subst he1 he2
     have h := ESubtyp.type_inv_subcapt hsub
@@ -33,21 +24,6 @@ theorem Typing.inv_subcapt
   (h : Typed Γ (Term.var x) (EType.type (CType.capt C S))) :
   Subcapt Γ {x} C :=
   Typing.inv_subcapt' rfl rfl h
-
--- theorem Typing.inv_var_exists'
---   (he1 : t0 = Term.var x) (he2 : E0 = EType.ex (CType.capt C.cweaken S))
---   (h : Typed Γ t0 E0) :
---   Subcapt Γ {x} C := by
---   induction h <;> try (solve | cases he1 | cases he2)
---   case var =>
---     cases he1; subst he2; apply Subcapt.evar; trivial
---   case sub hs ih =>
---     subst he1 he2
---     sorry
-
--- theorem Typing.inv_var_exists
---   (h : Typed Γ (Term.var x) (EType.ex (CType.capt C.cweaken S))) :
---   Subcapt Γ {x} C := sorry
 
 theorem Typed.bound_typing
   (hb : Context.Bound Γ x T) :
