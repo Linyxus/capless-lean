d81f93d9de8f4eef357d86875472ac7016363c48
Yichen Xu <yichen.x@outlook.com>
2024-08-01 21:49:22 +0100

    progress: setup theorem

--- Capless/Soundness/Progress.lean
+++ Capless/Soundness/Progress.lean
@@ -1,3 +1,31 @@
+import Capless.Reduction
 namespace Capless
+
+inductive Progress : State n m k -> Prop where
+| mk :
+  Reduce state state' ->
+  Progress state
+
+theorem progress
+  (ht : TypedState state E) :
+  Progress state := by
+  cases ht
+  case mk hs ht hc =>
+    cases ht
+    case var => sorry
+    case pack => sorry
+    case sub => sorry
+    case abs => sorry
+    case tabs => sorry
+    case cabs => sorry
+    case app => sorry
+    case tapp => sorry
+    case capp => sorry
+    case box => sorry
+    case unbox => sorry
+    case letin => sorry
+    case letex => sorry
+    case bindt => sorry
+    case bindc => sorry
 
 end Capless
