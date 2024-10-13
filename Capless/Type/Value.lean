64eba34852c5ea1303bc862b0c7714bea7a9672f
Yichen Xu <yichen.x@outlook.com>
2024-10-13 12:28:41 +0200

    use some notations in IsValue.lean

--- Capless/Type/Value.lean
+++ Capless/Type/Value.lean
@@ -3,16 +3,16 @@ namespace Capless
 
 @[aesop safe [constructors, cases]]
 inductive SType.IsValue : SType n m k -> Prop where
-| xforall : SType.IsValue (SType.forall T U)
-| tforall : SType.IsValue (SType.tforall S T)
-| cforall : SType.IsValue (SType.cforall T)
-| box : SType.IsValue (SType.box T)
+| xforall : SType.IsValue (∀(x:T)U)
+| tforall : SType.IsValue (∀[X<:S]T)
+| cforall : SType.IsValue (∀[c]T)
+| box : SType.IsValue (□ T)
 
 @[aesop safe [constructors, cases]]
 inductive CType.IsValue : CType n m k -> Prop where
 | capt :
-  (SType.IsValue S) ->
-  CType.IsValue (CType.capt C S)
+  S.IsValue ->
+  CType.IsValue (S^C)
 
 inductive SType.IsVar : SType n m k -> Prop where
 | tvar : SType.IsVar (SType.tvar X)
