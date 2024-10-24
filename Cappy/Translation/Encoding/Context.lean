3b4a6b028bd7fb3f7bb30d7c3f635de4b4097266
Yichen Xu <yichen.x@outlook.com>
2024-10-24 21:28:16 +0200

    cappy: typectx encoding

--- Cappy/Translation/Encoding/Context.lean
+++ Cappy/Translation/Encoding/Context.lean
@@ -5,5 +5,13 @@ namespace Cappy
 inductive Context.Interp : Context n m -> TContext n m k -> Prop where
 | empty :
   Context.Interp empty TContext.empty
+| var {Γ : Context n m} {Δ : TContext n m k} :
+  Context.Interp Γ Δ ->
+  CType.Interp (Δ.map.cweaken) Γ T {c=0} T' ->
+  Context.Interp (Γ.var T) (Δ.var T')
+| tvar {Γ : Context n m} {Δ : TContext n m k} :
+  Context.Interp Γ Δ ->
+  CType.Interp (Δ.map.cweaken) Γ (S^{}) {c=0} (S'^{}) ->
+  Context.Interp (Γ.tvar S) (Δ.tvar S')
 
 end Cappy
