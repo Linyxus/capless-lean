a35de676b4b24a8a145cef01704a3e1b30808a9c
Yichen Xu <yichen.x@outlook.com>
2024-10-28 15:11:14 +0100

    cappy: restructure again

--- Cappy/Translation/Encoding/CaptureSet/Core.lean
+++ Cappy/Translation/Encoding/CaptureSet/Core.lean
@@ -1,35 +1,35 @@
-import Cappy.Translation.Encoding.Context
-import Cappy.TypeSystem.Subcapturing
-import Capless.Subcapturing
-import Capless.Subcapturing.Basic
+import Cappy.Syntax
+import Capless.CaptureSet
+import Cappy.Translation.TContext
 namespace Cappy
 
-theorem subcapt_enc_monotonic
-  (hg : Context.Interp Γ ⟨Δ, ρ⟩)
-  (h : Δ ⊢ D1 <:c D2)
-  (hi1 : CaptureSet.Interp ρ Γ C D1 I1)
-  (hi2 : CaptureSet.Interp ρ Γ C D2 I2) :
-  (Δ ⊢ I1 <:c I2) := by
-  induction C generalizing I1 I2
-  case empty =>
-    cases hi1; cases hi2
-    apply Capless.Subcapt.refl
-  case union C1 C2 ih1 ih2 =>
-    cases hi1; cases hi2
-    apply Capless.Subcapt.join
-    { aesop }
-    { aesop }
-  case singleton =>
-    cases hi1; cases hi2
-    rename_i hb1 hi1 _ _ hb2 hi2
-    have h := Context.bound_inj hb1 hb2
-    cases h
-    sorry
-  case reach =>
-    cases hi1; cases hi2
-    apply Capless.Subcapt.refl
-  case universal =>
-    cases hi1; cases hi2
-    assumption
+/-!
+# Encoding of Capture Sets
+
+The `CaptureSet.Interp` derivation defines the encoding of capture sets from Cappy to Capless. It is a 5-place judgement:
+1. The mapping from reach capabilities to capture variables
+2. The type context in Cappy
+3. The input capture set
+4. The interpretation
+5. The output capture set
+!-/
+
+inductive CaptureSet.Interp : TMap n m k -> Context n m -> CaptureSet n -> Capless.CaptureSet n k -> Capless.CaptureSet n k -> Prop where
+| i_empty :
+  CaptureSet.Interp ρ Γ {} D {}
+| i_union :
+  CaptureSet.Interp ρ Γ C1 D D1 ->
+  CaptureSet.Interp ρ Γ C2 D D2 ->
+  CaptureSet.Interp ρ Γ (C1 ∪ C2) D (D1 ∪ D2)
+| i_singleton :
+  Context.Bound Γ x (S^C) ->
+  CaptureSet.Interp ρ Γ C {c=ρ.reach x} C' ->
+  CaptureSet.Interp ρ Γ {x=x} D C'
+| i_reach :
+  CaptureSet.Interp ρ Γ {x*=x} D {c=ρ.reach x}
+| i_cap :
+  CaptureSet.Interp ρ Γ {cap} D D
+
+notation:20 "⟦" C1 "⟧^" D " ↝c[" ρ " | " Γ "] " C2 => CaptureSet.Interp ρ Γ C1 D C2
 
 end Cappy
--- Cappy/Translation/Encoding/CaptureSet/Properties.lean
+++ Cappy/Translation/Encoding/CaptureSet/Properties.lean
@@ -1,4 +1,9 @@
 import Cappy.Translation.Encoding.CaptureSet.Core
 namespace Cappy
 
+theorem CaptureSet.interp_inj
+  (hi1 : CaptureSet.Interp ρ Γ C D I1)
+  (hi2 : CaptureSet.Interp ρ Γ C D I2) :
+  I1 = I2 := sorry
+
 end Cappy
