f5120978b8ecd08890f56a34c280a5327a7a7196
Yichen Xu <yichen.x@outlook.com>
2024-10-28 16:44:03 +0100

    cappy: capset interp is complete, attempt 1

--- Cappy/Translation/Encoding/CaptureSet/Properties.lean
+++ Cappy/Translation/Encoding/CaptureSet/Properties.lean
@@ -14,4 +14,93 @@ theorem CaptureSet.interp_inj
     have h := Context.bound_inj hb1 hb2
     aesop
 
+theorem CaptureSet.weaken_union {C1 C2 : CaptureSet n} :
+  (C1 ∪ C2).weaken = C1.weaken ∪ C2.weaken := by
+  simp [CaptureSet.weaken, CaptureSet.rename]
+
+theorem CType.weaken_capt :
+  (S^C : CType n m).weaken = S.weaken^C.weaken := by
+  simp [CType.weaken, CType.rename, CaptureSet.weaken, SType.weaken, RenameFun.weaken]
+
+theorem Capless.CaptureSet.weaken_union {C1 C2 : Capless.CaptureSet n k} :
+  (C1 ∪ C2).weaken = C1.weaken ∪ C2.weaken := by
+  simp [Capless.CaptureSet.weaken, Capless.CaptureSet.rename]
+
+theorem Capless.CaptureSet.cweaken_union {C1 C2 : Capless.CaptureSet n k} :
+  (C1 ∪ C2).cweaken = C1.cweaken ∪ C2.cweaken := by
+  simp [Capless.CaptureSet.cweaken, Capless.CaptureSet.crename]
+
+theorem Capless.CaptureSet.weaken_csingleton :
+  ({c=x} : Capless.CaptureSet n k).weaken = {c=x} := by
+  simp [Capless.CaptureSet.weaken, Capless.CaptureSet.rename]
+
+theorem TMap.ext_reach_succ {ρ : TMap n m k} :
+  ρ.ext.reach x.succ = (ρ.reach x).succ := by
+  simp [TMap.ext, Capless.FinFun.ext]
+
+theorem CaptureSet.interp_weaken
+  (hi : CaptureSet.Interp ρ Γ C D I) :
+  CaptureSet.Interp ρ.ext (Γ.var P) C.weaken D.cweaken.weaken I.cweaken.weaken := by
+  induction hi
+  case i_empty =>
+    simp [CaptureSet.weaken, CaptureSet.rename]
+    simp [Capless.CaptureSet.weaken, Capless.CaptureSet.rename]
+    constructor
+  case i_union ih1 ih2 =>
+    rw [CaptureSet.weaken_union]
+    rw [Capless.CaptureSet.cweaken_union, Capless.CaptureSet.weaken_union]
+    constructor <;> assumption
+  case i_singleton hb hi0 ih =>
+    simp [CaptureSet.weaken, CaptureSet.rename, Capless.FinFun.weaken]
+    have hb1 := Context.Bound.there_var (T' := P) hb
+    constructor
+    exact hb1
+    rw [TMap.ext_reach_succ]
+    rw [Capless.CaptureSet.cweaken_csingleton] at ih
+    rw [Capless.CaptureSet.weaken_csingleton] at ih
+    exact ih
+  case i_reach =>
+    simp [CaptureSet.weaken, CaptureSet.rename]
+    constructor
+  case i_cap =>
+    simp [CaptureSet.weaken, CaptureSet.rename]
+    constructor
+
+theorem CaptureSet.interp_complete :
+  ∃ I, CaptureSet.Interp ρ Γ C D I := by
+  rename_i n m k
+  induction n generalizing m k
+  case zero =>
+    induction C generalizing D
+    case empty => constructor; constructor
+    case union ih1 ih2 =>
+      have ⟨I1, ih1⟩ := ih1 (D := D)
+      have ⟨I2, ih2⟩ := ih2 (D := D)
+      constructor; constructor <;> assumption
+    case singleton x0 => apply Fin.elim0; assumption
+    case reach => constructor; constructor
+    case universal => constructor; constructor
+  case succ n0 ih =>
+    induction C generalizing D
+    case empty => constructor; constructor
+    case union ih1 ih2 =>
+      have ⟨I1, ih1⟩ := ih1 (D := D)
+      have ⟨I2, ih2⟩ := ih2 (D := D)
+      constructor; constructor <;> assumption
+    case singleton x0 =>
+      cases x0 using Fin.cases
+      case zero =>
+        cases Γ; rename_i Γ0 P
+        cases P; rename_i C S
+        have hb0 := Context.Bound.here (Γ := Γ0) (T := (S^C))
+        rw [CType.weaken_capt] at hb0
+        constructor
+        constructor
+        { exact hb0 }
+        { sorry }
+        all_goals sorry
+      case succ x0 => sorry
+    case reach => constructor; constructor
+    case universal => constructor; constructor
+
 end Cappy
