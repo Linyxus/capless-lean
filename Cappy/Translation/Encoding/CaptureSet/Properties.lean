b9ab9c162f93f70a4835ee6e60dd02dc985f54b2
Yichen Xu <yichen.x@outlook.com>
2024-10-28 15:16:23 +0100

    cappy: props of capture set interp

--- Cappy/Translation/Encoding/CaptureSet/Properties.lean
+++ Cappy/Translation/Encoding/CaptureSet/Properties.lean
@@ -4,6 +4,14 @@ namespace Cappy
 theorem CaptureSet.interp_inj
   (hi1 : CaptureSet.Interp ρ Γ C D I1)
   (hi2 : CaptureSet.Interp ρ Γ C D I2) :
-  I1 = I2 := sorry
+  I1 = I2 := by
+  induction hi1 generalizing I2 <;> try (solve | cases hi2; trivial)
+  case i_union ih1 ih2 =>
+    cases hi2
+    aesop
+  case i_singleton hb1 _ ih =>
+    cases hi2; rename_i hb2 _
+    have h := Context.bound_inj hb1 hb2
+    aesop
 
 end Cappy
