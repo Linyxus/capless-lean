4f77c6f90060465e13181ab99d32a6449a1e5219
Yichen Xu <yichen.x@outlook.com>
2025-01-16 20:56:54 +0800

    def: less-permissive

--- Capybara/Syntax/Mode.lean
+++ Capybara/Syntax/Mode.lean
@@ -16,9 +16,13 @@ notation "drop" => Mode.drop
 /-!
 Given `m1` and `m2`, `LessPermissive m1 m2` means that `m1` is less permissive than `m2`.
 -/
-inductive LessPermissive : Mutability -> Mutability -> Prop where
+inductive Mutability.LessPermissive : Mutability -> Mutability -> Prop where
 | readonly : LessPermissive Mutability.readonly Mutability.default
 | refl : LessPermissive m m
 
+inductive Mode.LessPermissive : Mode -> Mode -> Prop where
+| M : Mutability.LessPermissive m1 m2 -> LessPermissive (Mode.M m1) (Mode.M m2)
+| drop : LessPermissive m Mode.drop
+| refl : LessPermissive m m
 
 end Capybara
