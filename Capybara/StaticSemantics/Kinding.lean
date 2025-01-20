0f9d3bd8ac084ff1318fcf750641f9ca1b6b4b92
Yichen Xu <yichen.x@outlook.com>
2025-01-20 11:08:20 +0800

    def: kinding

--- Capybara/StaticSemantics/Kinding.lean
+++ Capybara/StaticSemantics/Kinding.lean
@@ -1,39 +1,18 @@
 import Capybara.Syntax
 import Capybara.StaticSemantics.CaptureRoot
+import Capybara.StaticSemantics.Separation
 namespace Capybara
 
-/-!
-Kinding of capture roots.
-- `empty` and `union` are structural rules
-- `k_ro`: a readonly access is immutable
-- `k_imm`: any access to an immutable capture set variable is immutable
-- `k_mut`: any access can be counted as mutable
-- `k_fresh`: access to a fresh capture set variable is fresh
--/
-inductive CaptureRoot.Kinding : Context n m k -> CaptureRoot k -> Kind -> Prop where
-| empty : CaptureRoot.Kinding Γ {} Kind.Imm
-| union :
-  CaptureRoot.Kinding Γ D1 K ->
-  CaptureRoot.Kinding Γ D2 K ->
-  CaptureRoot.Kinding Γ (D1 ∪ D2) K
-| k_ro :
-  CaptureRoot.Kinding Γ (CaptureRoot.singleton c ro) Kind.Imm
-| k_imm :
-  Context.LookupC Γ c (cparam Kind.Imm) ->
-  CaptureRoot.Kinding Γ (CaptureRoot.singleton c m) Kind.Imm
-| k_mut :
-  CaptureRoot.Kinding Γ (CaptureRoot.singleton c m) Kind.Mut
-| k_fresh :
-  Context.LookupC Γ c (cparam Kind.Fresh) ->
-  CaptureRoot.Kinding Γ (CaptureRoot.singleton c m) Kind.Fresh
-
-/-!
-Kinding of capture sets is defined by the kinding of its root capture variables.
--/
-inductive CaptureSet.Kinding : Context n m k -> CaptureSet n k -> Kind -> Prop where
-| mk :
-  CaptureSet.Root C Γ D ->
-  D.Kinding Γ K ->
-  CaptureSet.Kinding Γ C K
+inductive CaptureSet.Kinding : Context n m k -> CaptureSet n k -> CKind n k -> Prop where
+| fresh :
+  (∀c m, ReachRoot Γ C ⟨Mode.M m,c⟩ -> Context.LookupC Γ c (cparam CKind.Fresh)) ->
+  CaptureSet.Kinding Γ C CKind.Fresh
+| sep_mut :
+  Separation Γ C D ->
+  CaptureSet.Kinding Γ C (CKind.Sep ⟨SepMode.Mut, D⟩)
+| sep_imm :
+  Separation Γ C D ->
+  (∀r, ReachRoot Γ C r -> RORoot Γ r) ->
+  CaptureSet.Kinding Γ C (CKind.Sep ⟨SepMode.Imm, D⟩)
 
 end Capybara
