7fa6f22171a4f94f7dff7a1661b3d56cd98ede17
Yichen Xu <yichen.x@outlook.com>
2025-01-20 11:11:01 +0800

    def: make chaining up-to-date

--- Capybara/StaticSemantics/Chaining.lean
+++ Capybara/StaticSemantics/Chaining.lean
@@ -7,7 +7,7 @@ namespace Capybara
 Specifically, anything that has been dropped from `C1` cannot be mentioned by `C2` any more.
 -/
 def CaptureSet.Chaining (Γ : Context n m k) (C1 C2 : CaptureSet n k) : Prop :=
-  ∀c, ReachRoot Γ C1 drop c -> ∀m, ReachRoot Γ C2 m c -> False
+  ∀c, ReachRoot Γ C1 ⟨drop,c⟩ -> ∀m, ReachRoot Γ C2 ⟨m,c⟩ -> False
 
 notation:50 Γ " ⊢ " C1 " >> " C2 => CaptureSet.Chaining Γ C1 C2
 
