051633859ef6849168e5d8830f94170c95068118
Yichen Xu <yichen.x@outlook.com>
2025-01-21 10:01:33 +0800

    def: application rules

--- Capybara/StaticSemantics/Typing.lean
+++ Capybara/StaticSemantics/Typing.lean
@@ -26,19 +26,54 @@ inductive Typed : CaptureSet n k -> Context n m k -> Term n m k -> EType n m k -
   Kinding Γ ({c:=c}) CKind.Fresh ->
   -----------------------------------------------------
   Typed C' Γ (Term.pack c x) (EType.ex T)
-| sub :
+| subc :
   Typed C Γ t E ->
   (Γ ⊢c C <: C') ->
-  (Γ ⊢e E <: E') ->
   -----------------------------------
-  Typed C' Γ t E'
+  Typed C' Γ t E
 | abs :
-  Typed (C.weaken ∪ CaptureSet.span x) (Γ,x:T) t E ->
+  Typed (C.weaken ∪ CaptureSet.span 0) (Γ,x:T) t E ->
   ------------------------------------------------------------
   Typed {} Γ (Term.lam T t) (EType.type (((x:T)->E)^[ε]C))
 | tabs :
   Typed C (Γ,X:tparam S) t E ->
   ------------------------------------------------------------
   Typed C Γ (Term.tlam S t) (EType.type (([X<:S]->E)^[ε]C))
+| cabs :
+  Typed C.cweaken (Γ,c:cparam (CKind.Sep D)) t E ->
+  ------------------------------------------------------------
+  Typed C Γ (Term.clam D t) (EType.type (([c:D]->E)^[ε]C))
+| fabs :
+  Typed (C.cweaken.weaken ∪ CaptureSet.span 0) ((Γ,c:cparam CKind.Fresh),x:T) t E ->
+  ------------------------------------------------------------
+  Typed C Γ (Term.flam T t) (EType.type (([c:Fresh](x:T)->E)^[ε]C))
+| app :
+  Typed C' Γ (Term.var x) (EType.type (((x:T)->E)^[ε]Cf)) ->
+  Typed C' Γ (Term.var y) (EType.type T0) ->
+  (Γ ⊢ T0 <: T) ->
+  Chaining Γ ({x:=x}) ({x:=y}) ->
+  Chaining Γ ({x:=y}) ({x:=x}) ->
+  ------------------------------------------------------------
+  Typed C' Γ (Term.app x y) (E.open y)
+| tapp :
+  Typed C' Γ (Term.var x) (EType.type (([X<:S]->E)^[ε]Cf)) ->
+  (Γ ⊢s (SType.tvar X) <: S) ->
+  ------------------------------------------------------------
+  Typed C' Γ (Term.tapp x X) (E.topen X)
+| capp :
+  Typed C' Γ (Term.var x) (EType.type (([c:D]->E)^[ε]Cf)) ->
+  Kinding Γ ({c:=c}) (CKind.Sep D) ->
+  ------------------------------------------------------------
+  Typed C' Γ (Term.capp x c) (E.copen c)
+| fapp :
+  Typed C' Γ (Term.var x) (EType.type (([c:Fresh](x:T)->E)^[ε]Cf)) ->
+  Typed C' Γ (Term.var y) (EType.type T0) ->
+  (Γ ⊢ T0 <: (T.copen c)) ->
+  RootDropped Γ ({c:=c}) C' ->
+  Kinding Γ ({c:=c}) CKind.Fresh ->
+  DroppedChaining Γ ({c:=c}) ({x:=x}) ->
+  ------------------------------------------------------------
+  Typed C' Γ (Term.fapp x c y) ((E.copen c).open y)
+
 
 end Capybara
--- Capybara/Syntax/Type/Opening.lean
+++ Capybara/Syntax/Type/Opening.lean
@@ -4,4 +4,13 @@ namespace Capybara
 def CType.copen (T : CType n m (k+1)) (c : Fin k) : CType n m k :=
   T.rename (Renaming.copen c)
 
+def EType.open (T : EType (n+1) m k) (x : Fin n) : EType n m k :=
+  T.rename (Renaming.open x)
+
+def EType.topen (T : EType n (m+1) k) (X : Fin m) : EType n m k :=
+  T.rename (Renaming.topen X)
+
+def EType.copen (T : EType n m (k+1)) (c : Fin k) : EType n m k :=
+  T.rename (Renaming.copen c)
+
 end Capybara
