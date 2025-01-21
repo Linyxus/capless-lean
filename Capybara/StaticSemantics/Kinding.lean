2ab6bdcd698ec1603c05e5b1088c57b01177715d
Yichen Xu <yichen.x@outlook.com>
2025-01-21 09:02:31 +0800

    def: typing wip

--- Capybara/StaticSemantics/Kinding.lean
+++ Capybara/StaticSemantics/Kinding.lean
@@ -3,16 +3,16 @@ import Capybara.StaticSemantics.CaptureRoot
 import Capybara.StaticSemantics.Separation
 namespace Capybara
 
-inductive CaptureSet.Kinding : Context n m k -> CaptureSet n k -> CKind n k -> Prop where
+inductive Kinding : Context n m k -> CaptureSet n k -> CKind n k -> Prop where
 | fresh :
   (∀c m, ReachRoot Γ C ⟨Mode.M m,c⟩ -> Context.LookupC Γ c (cparam CKind.Fresh)) ->
-  CaptureSet.Kinding Γ C CKind.Fresh
+  Kinding Γ C CKind.Fresh
 | sep_mut :
   Separation Γ C D ->
-  CaptureSet.Kinding Γ C (CKind.Sep ⟨SepMode.Mut, D⟩)
+  Kinding Γ C (CKind.Sep ⟨SepMode.Mut, D⟩)
 | sep_imm :
   Separation Γ C D ->
   (∀r, ReachRoot Γ C r -> RORoot Γ r) ->
-  CaptureSet.Kinding Γ C (CKind.Sep ⟨SepMode.Imm, D⟩)
+  Kinding Γ C (CKind.Sep ⟨SepMode.Imm, D⟩)
 
 end Capybara
--- Capybara/StaticSemantics/Typing.lean
+++ Capybara/StaticSemantics/Typing.lean
@@ -5,32 +5,40 @@ import Capybara.StaticSemantics.Subtyping
 import Capybara.StaticSemantics.Kinding
 namespace Capybara
 
+/-!
+`RootDropped Γ C D` means that any capture root in `C` is dropped in `D`.
+-/
+def RootDropped (Γ : Context n m k) (C D : CaptureSet n k) : Prop :=
+  ∀ m c, ReachRoot Γ C ⟨m,c⟩ -> (Γ ⊢c {c@drop:=c} <: D)
+
+/-!
+The typing relation.
+-/
 inductive Typed : CaptureSet n k -> Context n m k -> Term n m k -> EType n m k -> Prop where
 | var :
   Γ.Lookup x (CType.capt C m S) ->
-  LessPermissive m' m ->
+  Mutability.LessPermissive m' m ->
   --------------------------------
   Typed ({x@Mode.M m':=x}) Γ (Term.var x) (EType.type (CType.capt ({x:=x}) m' S))
 | pack :
   Typed C' Γ (Term.var x) (EType.type (T.copen c)) ->
-  CaptureSet.Root ({c:=c}) Γ R ->
-  CaptureRoot.Kinding Γ R Kind.Fresh ->
-  CaptureRoot.Dropped R C' ->
+  RootDropped Γ ({c:=c}) C' ->
+  Kinding Γ ({c:=c}) CKind.Fresh ->
   -----------------------------------------------------
   Typed C' Γ (Term.pack c x) (EType.ex T)
 | sub :
   Typed C Γ t E ->
   (Γ ⊢c C <: C') ->
   (Γ ⊢e E <: E') ->
-  --------------------------------
+  -----------------------------------
   Typed C' Γ t E'
 | abs :
   Typed (C.weaken ∪ CaptureSet.span x) (Γ,x:T) t E ->
-  --------------------------------
+  ------------------------------------------------------------
   Typed {} Γ (Term.lam T t) (EType.type (((x:T)->E)^[ε]C))
 | tabs :
   Typed C (Γ,X:tparam S) t E ->
-  ---------------------------------
+  ------------------------------------------------------------
   Typed C Γ (Term.tlam S t) (EType.type (([X<:S]->E)^[ε]C))
 
 end Capybara
