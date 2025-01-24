7654685ef2b3d52ebccfa141825bb76f959094fd
Yichen Xu <yichen.x@outlook.com>
2025-01-24 17:38:33 +0800

    I am so sorry

--- Capless/Tactics.lean
+++ Capless/Tactics.lean
@@ -1,4 +1,5 @@
 macro "easy" : tactic => `(tactic| assumption)
+macro "sosorry" : tactic => `(tactic| all_goals sorry)
 macro "split_and" : tactic => `(tactic| repeat any_goals apply And.intro)
 macro "apply!" e:term : tactic => `(tactic| apply $e <;> easy)
 macro "apply?" e:term : tactic => `(tactic| apply $e <;> try easy)
