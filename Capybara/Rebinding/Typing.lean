4437ef1ecd01d7e7a1a8a30bd4c4cd6b727eae5e
Yichen Xu <yichen.x@outlook.com>
2025-01-25 13:20:18 +0800

    proof(rebind): typing scaffold

--- Capybara/Rebinding/Typing.lean
+++ Capybara/Rebinding/Typing.lean
@@ -2,4 +2,36 @@ import Capybara.StaticSemantics
 import Capybara.Rebinding.Subtyping
 namespace Capybara
 
+theorem Typed.rebind {Γ : Context n m k} {Δ : Context n' m' k'}
+  (h : Typed C Γ t E)
+  (θ : Rebinding Γ Δ) :
+  Typed (C.rename θ.ρ.asCapt) Δ (t.rename θ.ρ) (E.rename θ.ρ) := by
+  induction h generalizing n' m' k' Δ
+  case var =>
+    sorry
+  case pack =>
+    sorry
+  case subc =>
+    sorry
+  case abs =>
+    sorry
+  case tabs =>
+    sorry
+  case cabs =>
+    sorry
+  case fabs =>
+    sorry
+  case app =>
+    sorry
+  case tapp =>
+    sorry
+  case capp =>
+    sorry
+  case fapp =>
+    sorry
+  case letin =>
+    sorry
+  case unpack =>
+    sorry
+
 end Capybara
