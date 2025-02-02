c2d30ced0c1e86a465322b4cf3584f1b3455b890
Yichen Xu <yichen.x@outlook.com>
2025-02-02 03:55:40 +0100

    proof(rebind): separation wip

--- Capybara/Rebinding/Separation.lean
+++ Capybara/Rebinding/Separation.lean
@@ -30,4 +30,11 @@ theorem RootSeparation.rebind
     { have hc2' := θ.cvar hc2; easy }
     apply θ.fresh_inj hc1 hc2 hneq
 
+theorem Separation.rebind
+  (h : Separation Γ C1 C2)
+  (θ : Rebinding Γ Δ) :
+  Separation Δ (C1.rename θ.ρ.asCapt) (C2.rename θ.ρ.asCapt) := by
+  intro r1 r2 h1 h2
+  sorry
+
 end Capybara
