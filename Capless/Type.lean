2ef69aa266e8d001965c55cb0f99b1297fa83c8d
Yichen Xu <yichen.x@outlook.com>
2025-06-24 15:22:07 +0200

    doc: types

--- Capless/Type.lean
+++ Capless/Type.lean
@@ -3,3 +3,17 @@ import Capless.Type.Value
 import Capless.Type.Renaming
 import Capless.Type.Basic
 import Capless.Type.Simp
+
+/-!
+# Types in System Capless
+
+This module defines the syntax of types (Fig. 1) in System Capless.
+
+## Submodules
+
+- [Core](/Capless/Type/Core.html): Core type syntax including capturing types, existential types, and shape types
+- [Value](/Capless/Type/Value.html): Predicates for identifying value types and type variables
+- [Renaming](/Capless/Type/Renaming.html): Variable, type variable, and capture variable renaming operations
+- [Basic](/Capless/Type/Basic.html): Fundamental properties and lemmas about type renaming operations
+- [Simp](/Capless/Type/Simp.html): Simplification lemmas for type operations used by the simplifier
+-/
