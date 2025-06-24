f5b2d285d86cbf85fa3ed54a3009aca30d387f5c
Yichen Xu <yichen.x@outlook.com>
2025-06-24 14:38:33 +0200

    Add docgen

--- lakefile.lean
+++ lakefile.lean
@@ -14,6 +14,12 @@ package «capless» where
 require mathlib from git
   "https://github.com/leanprover-community/mathlib4.git"
 
+require «doc-gen4» from git
+  "https://github.com/leanprover/doc-gen4" @ "v4.21.0-rc3"
+
+require «importGraph» from git -- requires graphviz
+  "https://github.com/leanprover-community/import-graph" @ "v4.21.0-rc3"
+
 @[default_target]
 lean_lib «Capless» where
   -- add any library configuration options here
